1aad2d0aa969931253d7cb27ba74628da29346e9
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-05-27 18:42:36 +0300

    Write the fetch_cat_facts method that fetches the catfacts from the given URL and test it

--- python_reflex_app/python_reflex_app.py
+++ python_reflex_app/python_reflex_app.py
@@ -1,6 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+import requests
 
 from rxconfig import config
 
@@ -17,6 +18,24 @@ def index() -> rx.Component:
                 our feline friends with Cat Fact Finder. From quirky behaviors to 
                 historical tidbits, our app provides a treasure trove of information 
                 about cats. Join us today and become a cat facts expert!"""
+    cat_facts_url = 'https://cat-fact.herokuapp.com/facts/'
+    status_code = 200
+
+    def fetch_cat_facts():
+        try:
+            response = requests.get(cat_facts_url)
+            if not response.status_code:
+                print('Error', response.status_code)
+                return None
+            else:
+                cat_facts = response.json()
+                return cat_facts
+        except requests.exceptions.RequestException as e:
+            print('Error', e)
+            return None            
+
+    print(fetch_cat_facts())
+
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
